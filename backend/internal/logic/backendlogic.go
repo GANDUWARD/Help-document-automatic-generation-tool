bfec10d274bc3b96afa9f2736acce9f74354917f
GANDUWARD <2635648098@qq.com>
2023-12-28 23:45:04 +0800

    读取基本后端文件结构

--- backend/internal/logic/backendlogic.go
+++ backend/internal/logic/backendlogic.go
@@ -2,9 +2,12 @@ package logic
 
 import (
 	"context"
+	"fmt"
+	"os"
 
 	"backend/internal/svc"
 	"backend/internal/types"
+	"path/filepath"
 
 	"github.com/zeromicro/go-zero/core/logx"
 )
@@ -25,6 +28,14 @@ func NewBackendLogic(ctx context.Context, svcCtx *svc.ServiceContext) *BackendLo
 
 func (l *BackendLogic) Backend(req *types.Request) (resp *types.Response, err error) {
 	// todo: add your logic here and delete this line
-
+	currentDir, _ := os.Getwd()
+	// 构建目标文件夹路径
+	targetDir := filepath.Join(currentDir, "taskarea")
+	//获取当前目录下的所有文件和目录信息
+	filepath.Walk(targetDir, func(path string, info os.FileInfo, err error) error {
+		fmt.Println(path)      //打印path信息
+		fmt.Println(info.Name) // 打印文件或目录名
+		return nil
+	})
 	return
 }
---
+++ backend/taskarea/example.c
@@ -0,0 +1,24 @@
+// File: example.c
+
+/**
+ * @file example.c
+ * @brief This is an example file.
+ */
+
+#include <iostream>
+
+/**
+ * @brief A simple function to add two numbers.
+ * @param a The first number.
+ * @param b The second number.
+ * @return The sum of a and b.
+ */
+int add(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    // ...
+
+    return 0;
+}
